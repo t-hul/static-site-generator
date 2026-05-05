df1ca5d389d373d529ce45b4978681a75091d154
t-hul <thomashultsch@web.de>
2026-05-05 12:04:41 +0200

    Add block_to_block_type() and tests for it

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,23 @@
+import re
+from enum import Enum
 from typing import List
+
+from parentnode import ParentNode
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def markdown_to_html_node(markdown: str) -> ParentNode:
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
 
 
 def markdown_to_blocks(markdown: str) -> List[str]:
@@ -9,3 +28,29 @@ def markdown_to_blocks(markdown: str) -> List[str]:
         if stripped_block != "":
             stripped_blocks.append(stripped_block)
     return stripped_blocks
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if re.match(r"^#{1,6}\s.+", block):
+        return BlockType.HEADING
+    if re.match(r"^```\n.+```$", block):
+        return BlockType.CODE
+    if re.fullmatch(r"^(?:\s*>\s?.+(?:\n|$))+", block):
+        return BlockType.QUOTE
+    if re.fullmatch(r"^(?:\s*-\s.+(?:\n|$))+", block):
+        return BlockType.UNORDERED_LIST
+    if _is_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+
+
+def _is_ordered_list(block: str) -> bool:
+    lines = block.splitlines()
+    numbers = []
+    for line in lines:
+        match = re.match(r"^(\d+)\.\s.+$", line.strip())
+        if not match:
+            return False
+        numbers.append(int(match.group(1)))
+
+    return numbers == list(range(1, len(numbers) + 1))
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -23,3 +23,184 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_markdown_to_blocks_many_newline(self):
+        md = """
+
+This is **bolded** paragraph
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+
+
+
+- This is a list
+- with items
+
+
+
+
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_markdown_to_blocks_single(self):
+        md = "This is **bolded** paragraph"
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            ["This is **bolded** paragraph"],
+        )
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading_single(self):
+        block = "# Heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_heading_no_space(self):
+        block = "#Heading"
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.HEADING)
+
+    def test_heading_level6(self):
+        block = "###### Heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_heading_level7(self):
+        block = "####### Heading"
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.HEADING)
+
+    def test_heading_different_start(self):
+        block = "s# Heading"
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.HEADING)
+
+    def test_code(self):
+        block = """```
+        test code```"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_code_no_newline(self):
+        block = """```test code```"""
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.CODE)
+
+    def test_quote(self):
+        block = """> one
+> two
+> three"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_quote_no_space(self):
+        block = """>one
+>two
+>three"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_quote_not_first(self):
+        block = """one
+> two
+> three"""
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.QUOTE)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_quote_not_second(self):
+        block = """> one
+two
+> three"""
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.QUOTE)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_unordered(self):
+        block = """- one
+- two
+- three"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_unordered_leading_space(self):
+        block = """- one
+                   - two
+                   - three"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_unordered_no_space(self):
+        block = """-one
+-two
+-three"""
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.UNORDERED_LIST)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_unordered_not_second(self):
+        block = """- one
+two
+- three"""
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.UNORDERED_LIST)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_ordered(self):
+        block = """1. one
+2. two
+3. three"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_ordered_leading_space(self):
+        block = """1. one
+                   2. two
+                   3. three"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_ordered_not_starting_at_one(self):
+        block = """2. one
+3. two
+4. three"""
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.ORDERED_LIST)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_ordered_not_increasing(self):
+        block = """1. one
+3. two
+3. three"""
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.ORDERED_LIST)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_ordered_no_dot(self):
+        block = """1. one
+2 two
+3. three"""
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.ORDERED_LIST)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_ordered_no_space(self):
+        block = """1. one
+2.two
+3. three"""
+        block_type = block_to_block_type(block)
+        self.assertNotEqual(block_type, BlockType.ORDERED_LIST)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
